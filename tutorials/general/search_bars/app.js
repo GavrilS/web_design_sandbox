d7fd1145bf1bba4fa72917875bb08c112c7f68a6
GavrilS <gavr1lll@abv.bg>
2025-07-24 08:45:29 +0300

    Adding search functionality

---
+++ tutorials/general/search_bars/app.js
@@ -0,0 +1,6 @@
+searchInput = document.getElementById('input-search');
+searchBtn = document.getElementById('search-btn');
+
+searchBtn.addEventListener('onclick', () => {
+    console.log('Search btn clicked!');
+});
