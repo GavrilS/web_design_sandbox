9f9704afed43567da664ec0c102b4338ed61c523
GavrilS <gavr1lll@abv.bg>
2025-07-24 21:44:13 +0300

    Search functionality is fully added

--- tutorials/general/search_bars/app.js
+++ tutorials/general/search_bars/app.js
@@ -1,12 +1,33 @@
 const searchBtn = document.getElementById('search-btn');
 const searchBox = document.getElementById('input-search');
+const infoBoxes = document.querySelectorAll('.info-container > div');
 
 searchBtn.addEventListener('click', () => {
+    clearHiddenClass();
     let searchTerm = searchBox.value;
     if (!searchTerm) {
         console.log('Search Term is not populated; skipping...');
     } else {
-        console.log('We are in action!');
+        infoBoxes.forEach((div) => {
+            let flag = false;
+            for (child of div.children) {
+                if (child.innerText.includes(searchTerm)) {
+                    flag = true;
+                    break;
+                }
+            }
+            if (!flag) {
+                div.classList.add('hidden');
+            }
+        })
     }
 });
+
+
+function clearHiddenClass() {
+    let hiddenBoxes = document.querySelectorAll('.hidden');
+    hiddenBoxes.forEach((div) => {
+        div.classList.remove('hidden');
+    });
+}
 
