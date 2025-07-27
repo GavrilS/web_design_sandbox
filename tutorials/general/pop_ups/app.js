3e3f765f93ebc3ff1b8f9143d35f1fc7a7718d1f
GavrilS <gavr1lll@abv.bg>
2025-07-27 14:02:01 +0300

    Added class to center the popup and make it visible when the respective button is clicked, also added function to handle the class change

--- tutorials/general/pop_ups/app.js
+++ tutorials/general/pop_ups/app.js
@@ -1,10 +1,29 @@
+const contactForm = document.getElementById('contact-form');
+const contactBtn = document.getElementById('contact-button');
+const signupForm = document.getElementById('signup-form');
+const signupBtn = document.getElementById('signup-button');
 
+contactBtn.addEventListener('onclick', showPopup(contactForm));
+signupBtn.addEventListener('onclick', showPopup(signupForm));
 
 
 function sendMessage() {
     console.log('Thank you for reaching out to our team!');
+
 }
 
 function signup() {
     console.log('Thank you for signing up!');
+}
+
+function showPopup(form) {
+    form.classList.add('popup-active');
+    console.log(`Adding class to element: ${form}`);
+    // if (form === 'contact') {
+    //     contactForm.classList.add('popup-active');
+    //     console.log('adding active class to contact');
+    // } else {
+    //     signupForm.classList.add('popup-active');
+    //     console.log('adding active class to signup');
+    // }
 }
